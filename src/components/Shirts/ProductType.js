5a9e06a73f8582e3aac1094675cfca2b3443ac50
filipjovicccc <filipjovicccc@gmail.com>
2022-12-13 14:10:07 +0100

    update

--- src/components/Shirts/ProductType.js
+++ src/components/Shirts/ProductType.js
@@ -1,20 +1,67 @@
 import React from "react";
 import "./ProductType.css";
+import { useState } from "react";
 
 function ProductType() {
+  // const [toggleActive, setToggleActive] = useState(false);
+
+  // const toggleChecked = () => {
+  //   setToggleActive(toggleActive);
+  // };
+
   return (
     <div className="products">
       <ul className="product-list">
         <h4> Tip proizvoda</h4>
-        <li>Muška majica</li>
-        <li>Ženska majica</li>
-        <li>Dečija majica - unisex</li>
-        <li>Duks - unisex</li>
-        <li>Duks basic - unisex</li>
-        <li>Duks premium - unisex</li>
-        <li>Bebi bodi kratak rukav</li>
-        <li>Bebi bodi dug rukav</li>
-        <li>Portika</li>
+        <li className="main-list">
+          <input checked="checked" type="checkbox" /> Muška majica
+          <span
+            // onClick={toggleChecked}
+            // className={`mark show`}
+            className="mark"
+          ></span>
+        </li>
+        <li className="main-list">
+          {" "}
+          <input checked="checked" type="checkbox" />
+          <span className="mark"></span>
+          Ženska majica
+        </li>
+        <li>
+          {" "}
+          <input className="products-check" type="checkbox" />
+          Dečija majica - unisex
+        </li>
+        <li>
+          {" "}
+          <input className="products-check" type="checkbox" />
+          Duks - unisex
+        </li>
+        <li>
+          {" "}
+          <input className="products-check" type="checkbox" />
+          Duks basic - unisex
+        </li>
+        <li>
+          {" "}
+          <input className="products-check" type="checkbox" />
+          Duks premium - unisex
+        </li>
+        <li>
+          {" "}
+          <input className="products-check" type="checkbox" />
+          Bebi bodi kratak rukav
+        </li>
+        <li>
+          {" "}
+          <input className="products-check" type="checkbox" />
+          Bebi bodi dug rukav
+        </li>
+        <li>
+          {" "}
+          <input className="products-check" type="checkbox" />
+          Portika
+        </li>
       </ul>
     </div>
   );
