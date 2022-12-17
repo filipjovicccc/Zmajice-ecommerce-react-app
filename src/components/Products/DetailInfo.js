b44d0169d4b34800b60f60d7372cd26cda942f18
filipjovicccc <filipjovicccc@gmail.com>
2022-12-17 22:40:00 +0100

    update

--- src/components/Products/DetailInfo.js
+++ src/components/Products/DetailInfo.js
@@ -4,12 +4,71 @@ import "./DetailInfo.css";
 function DetailInfo() {
   return (
     <div>
-      <h2>Informacije o proizvodu</h2>
-      <div className="detail-box">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="detail-wrapper">
+        <div className="detail-box">
+          <h3>Muška majica</h3>
+          <ul>
+            <li>
+              100% pamuk, Sing Umbro 160g/m2 - pamuk visokog
+              <br />
+              kvaliteta
+            </li>
+            <li>Standardne veličine i ravan kroj</li>
+            <li>
+              Direktna digitalna štampa (DTG) - najsavremenija <br />
+              tehnika štampe
+            </li>
+            <li>Proizvedeno u Srbiji</li>
+          </ul>
+        </div>
+        <div className="detail-box center">
+          <h1>MUŠKA MAJICA</h1>
+          <table>
+            <tr>
+              <th></th>
+              <th>XS</th>
+              <th>S</th>
+              <th>M</th>
+              <th>L</th>
+              <th>XL</th>
+              <th>XXL</th>
+              <th>3XL</th>
+            </tr>
+            <tr>
+              <td>ŠIRINA</td>
+              <td>50</td>
+              <td>52</td>
+              <td>54</td>
+              <td>57</td>
+              <td>60</td>
+              <td>62</td>
+              <td>64</td>
+            </tr>
+            <tr>
+              <td>DUŽINA</td>
+              <td>67</td>
+              <td>69</td>
+              <td>72</td>
+              <td>74</td>
+              <td>76</td>
+              <td>78</td>
+              <td>80</td>
+            </tr>
+          </table>
+        </div>
+        <div className="detail-box">
+          <h3>Upustvo za održanje proizvoda</h3>
+          <p>
+            Proizvod prati izvrnut na temperaturi ne većoj od 40 stepeni <br />
+            celzijusa, kako bi se osigurali da neće doći do oštećenja printa.
+            <br />
+            <br />
+            Proizvod peglati izvrnut, kako bi se osigurali da neće doći do
+            oštećenja printa. <br />
+            Moguće sakupljanje proizvoda usled pranja do 2-3%
+          </p>
+        </div>
+        <div className="detail-box"></div>
       </div>
     </div>
   );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { Fragment } from "react";
 import { BiPlus } from "react-icons/bi";
 import { useCart } from "react-use-cart";
@@ -7,6 +7,21 @@ import "./Product.css";
 
 function Products(props) {
   const { addItem } = useCart();
+
+  const [toggleInfo, setToggleInfo] = useState(false);
+  const [toggleDelivery, setToggleDelivery] = useState(false);
+  const [toggleReclamation, setToggleReclamation] = useState(false);
+
+  const toggleActiveInfo = () => {
+    setToggleInfo(!toggleInfo);
+  };
+  const toggleActiveDelivery = () => {
+    setToggleDelivery(!toggleDelivery);
+  };
+
+  const toggleActiveReclamation = () => {
+    setToggleReclamation(!toggleReclamation);
+  };
 
   const circleStyle = props.color;
   const borderStyle = "0.2px solid gray";
@@ -21,8 +36,6 @@ function Products(props) {
           <h1 id="product-header">{props.title}</h1>
           <p id="product-id">#{props.id}</p>
           <h4 className="price-font">{props.price}RSD</h4>
-          {/* <h5 className="product-font">{props.desc}</h5> */}
-          {/* {props.quantity} */}
           <div>
             <h5 className="product-font">BOJA</h5>
 
@@ -67,16 +80,28 @@ function Products(props) {
       <section className="bottom-info">
         <div className="more-info">
           <h4>Informacije o proizvodu</h4>
-          <BiPlus style={plusStyle} />
+          <BiPlus onClick={toggleActiveInfo} style={plusStyle} />
         </div>
+        {toggleInfo && <DetailInfo />}
+
         <div className="more-info">
           <h4>Dostava</h4>
-          <BiPlus style={plusStyle} />
+          <BiPlus onClick={toggleActiveDelivery} style={plusStyle} />
         </div>
+        {toggleDelivery && (
+          <div>
+            <h1>Hello from delivery</h1>
+          </div>
+        )}
         <div className="more-info">
           <h4>Reklamacije</h4>
-          <BiPlus style={plusStyle} />
+          <BiPlus onClick={toggleActiveReclamation} style={plusStyle} />
         </div>
+        {toggleReclamation && (
+          <div>
+            <h1>Hello from reclamation</h1>
+          </div>
+        )}
       </section>
     </Fragment>
   );
